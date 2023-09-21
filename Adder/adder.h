1794e58fe71078578949a2c3bbb49459f1ffec4d
octieuhp <octieugm@gmail.com>
2023-09-22 01:21:48 +0700

    init

---
+++ Adder/adder.h
@@ -0,0 +1,5 @@
+#pragma once
+namespace mearlymath {
+	int add(int a,int b);
+	float add(float a, float b);
+}
---
+++ TestSome/cpp/main.cpp
@@ -0,0 +1,7 @@
+#include "adder.h"
+#include <iostream>
+int main(){
+	std::cout << "2 + 3 = " << mearlymath::add(2,3) << '\n';
+	return 0;
+	
+}
